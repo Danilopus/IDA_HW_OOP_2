1881ccd636c6c1899cc5cdab7fe5c34c4b19e244
Ишутинов Данил <gladius@inbox.ru>
2023-06-10 20:20:17 +0500

    definition reservoirs moved to cpp

--- IDA_HW_OOP_2/Reservoir.cpp
+++ IDA_HW_OOP_2/Reservoir.cpp
@@ -21,6 +21,21 @@ Reservoir::Reservoir(Reservoir copy_obj, int index)
 	_index = index;
 }
 
+void Reservoir::Set_type()
+{
+	double Surface_area = Get_Surface_area();
+	if (Surface_area > 1e12)
+		_parametrs.type_code = 1;
+	else if (Surface_area > 1e10)
+		_parametrs.type_code = 2;
+	else if (Surface_area > 1e4)
+		_parametrs.type_code = 3;
+	else if (Surface_area > 1e2)
+		_parametrs.type_code = 4;
+	else
+		_parametrs.type_code = 5;
+}
+
 void Reservoir::Reservoir_fabric(int Current_Reservoirs_amount, Reservoir Reservoirs_dataset[], Reservoir_fabric_parametrs fabric_parametrs)
 {
 	Reservoir_parametrs random_parametrs;
@@ -130,6 +145,87 @@ void Reservoir::ShowInfoSummary(int Current_Reservoirs_amount, Reservoir Reservo
 		std::cout << "\nReservoir " << i + 1;
 		Reservoirs_dataset[i].ShowInfo_obj();
 	}
+}
+
+void const Reservoir::ShowInfo_obj()
+{
+	std::cout << "\n" << "name:          " << _name;
+	std::cout << "\n" << "type:          " << Get_type();
+	std::cout << "\n" << "depth:         " << _parametrs.depth << " meters";
+	std::cout << "\n" << "surface area:  " << Get_Surface_area() << " sqr meters";
+	std::cout << "\n" << "approx volume: " << Get_Volume_approx() << " cubic meters";
+	std::cout << "\n";
+}
+
+std::string Reservoir::Get_type()
+{
+	switch (_parametrs.type_code)
+	{
+	case 1: return "ocean";
+	case 2: return "sea";
+	case 3: return "lake";
+	case 4: return "pool";
+	case 5: return "small reservoir";
+	}
+	return "unknown";
+}
+
+void Reservoir::UserChoiceHandle(int& Current_Reservoirs_amount, Reservoir*& Reservoirs_dataset)
+{
+	int selected_method = Get_Int_Positive();
+	switch (selected_method)
+	{
+	case 1:	Reservoir::ShowInfoSummary(Current_Reservoirs_amount, Reservoirs_dataset);  break;
+	case 3: Reservoir::Delete_reservoir(Current_Reservoirs_amount, Reservoirs_dataset); break;
+	case 2: Reservoir::Add_reservoir(Current_Reservoirs_amount, Reservoirs_dataset); break;
+	case 4: Reservoir::Copy_reservoir(Current_Reservoirs_amount, Reservoirs_dataset); break;
+	case 5: Reservoir::Save_to_file(Current_Reservoirs_amount, Reservoirs_dataset, ".txt"); break;
+	case 6: Reservoir::Save_to_file(Current_Reservoirs_amount, Reservoirs_dataset, ".bin"); break;
+
+	}
+}
+
+void Reservoir::Save_to_file(int Current_Reservoirs_amount, Reservoir* Reservoirs_dataset, std::string operation_type)
+{
+	std::string file_name;
+	std::cout << "\nEnter a file name ->";
+	getline(std::cin, file_name);
+	if (file_name == "") file_name = "Reservoir_dataset";
+	file_name += operation_type;
+	std::ofstream file_stream_obj;
+	if (operation_type == ".txt")
+		file_stream_obj.open(file_name);
+	else if (operation_type == ".bin")
+		file_stream_obj.open(file_name, std::ios::binary);
+
+	if (file_stream_obj.is_open())
+	{
+		file_stream_obj << Summary_Info_to_string(Current_Reservoirs_amount, Reservoirs_dataset);
+	}
+	file_stream_obj.close();
+	std::cout << "\nData saved to " << file_name << " - OK\n";
+}
+
+std::string Reservoir::Summary_Info_to_string(int Current_Reservoirs_amount, Reservoir Reservoirs_dataset[])
+{
+	std::string tmp_str;
+	for (int i = 0; i < Current_Reservoirs_amount; i++)
+	{
+		tmp_str += "\nReservoir " + std::to_string(i + 1) + Reservoirs_dataset[i].Obj_Info_to_string();
+	}
+	return tmp_str;
+}
+
+std::string Reservoir::Obj_Info_to_string()
+{
+	std::string tmp_str;
+	//tmp_str = "\n" + "name:          " + _name
+	tmp_str = "\nname:          " + _name
+		+ "\n" + "type:          " + Get_type()
+		+ "\n" + "depth:         " + std::to_string(_parametrs.depth)
+		+ "\n" + "surface area:  " + std::to_string(Get_Surface_area()) + " sqr m"
+		+ "\n" + "approx volume: " + std::to_string(Get_Volume_approx()) + " cubic m" + "\n";
+	return tmp_str;
 }
 
 void Reservoir::Delete_reservoir(int& Current_Reservoirs_amount, Reservoir*& Reservoirs_dataset)
